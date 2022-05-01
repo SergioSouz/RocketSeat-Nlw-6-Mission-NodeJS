eb07039f3b83cd17a8b7bcd3cfe2e1afabc38633
Sergio Souz <sergio.dev0208@gmail.com>
2022-05-01 07:41:10 -0300

    criando banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,8 @@
 import express from 'express'
+import "reflect-metadata";
+import "./database"
 
 const app = express()
-
-/**
- * 
- * ============TIPOS DE METODOS =================
- * 
- * GET     =>  Buscar
- * POST    =>  Inserir
- * DELETE  =>  Remover
- * PUT     =>  Alterar
- * PUTCH   =>  Alterar dado espercifico 
- * 
- * ==============================================
- */
-
-
-
-/**
- * 
- * Req => response => entrando 
- * Res => resposta => saindo
- * 
- */
-
-
-app.get("/test", (req,res)=>{
-    return res.send("Primeira rota get")
-})
-
-
-app.post("/test-post", (req,res)=>{
-    return res.send("Primeira rota post")
-})
-
 
 // http://localhost:3000
 app.listen(3000, () => console.log("SERVER IS RUNNING"))
